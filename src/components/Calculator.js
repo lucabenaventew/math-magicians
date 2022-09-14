d369cf9e71b4f34098f227ba92d0822045d8fd7a
lucabenaventew <cacachuuuluca@gmail.com>
2022-09-14 16:35:26 -0500

    Update components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,44 +14,48 @@ function Calculator() {
       result.value = newVal.total;
     }
   };
+
   return (
-    <div className="wrapper">
-      <form>
-        <input name="input" type="text" id="input" />
-      </form>
-      <table className="keyboard-wrapper">
-        <tbody className="keyboard">
-          <tr>
-            <th><button type="button" className="AC" onClick={input}>AC</button></th>
-            <th><button type="button" className="plus-minus" onClick={input}>+/-</button></th>
-            <th><button type="button" className="percent" onClick={input}>%</button></th>
-            <th><button type="button" className="divide" onClick={input}>÷</button></th>
-          </tr>
-          <tr>
-            <th><button type="button" className="one" onClick={input}>1</button></th>
-            <th><button type="button" className="two" onClick={input}>2</button></th>
-            <th><button type="button" className="three" onClick={input}>3</button></th>
-            <th><button type="button" className="multiply" onClick={input}>x</button></th>
-          </tr>
-          <tr>
-            <th><button type="button" className="four" onClick={input}>4</button></th>
-            <th><button type="button" className="five" onClick={input}>5</button></th>
-            <th><button type="button" className="six" onClick={input}>6</button></th>
-            <th><button type="button" className="minus" onClick={input}>-</button></th>
-          </tr>
-          <tr>
-            <th><button type="button" className="seven" onClick={input}>7</button></th>
-            <th><button type="button" className="eight" onClick={input}>8</button></th>
-            <th><button type="button" className="nine" onClick={input}>9</button></th>
-            <th><button type="button" className="plus" onClick={input}>+</button></th>
-          </tr>
-          <tr>
-            <th colSpan={2}><button type="button" className="zero" onClick={input}>0</button></th>
-            <th><button type="button" className="dot" onClick={input}>.</button></th>
-            <th><button type="button" className="equal" onClick={input}>=</button></th>
-          </tr>
-        </tbody>
-      </table>
+    <div className="calculator-wrapper">
+      <h2 className="calculator-title">Lets do some Math!</h2>
+      <div className="calculator">
+        <form>
+          <input name="input" type="text" id="input" />
+        </form>
+        <table className="keyboard-wrapper">
+          <tbody className="keyboard">
+            <tr>
+              <th><button type="button" className="AC" onClick={input}>AC</button></th>
+              <th><button type="button" className="plus-minus" onClick={input}>+/-</button></th>
+              <th><button type="button" className="percent" onClick={input}>%</button></th>
+              <th><button type="button" className="divide" onClick={input}>÷</button></th>
+            </tr>
+            <tr>
+              <th><button type="button" className="one" onClick={input}>1</button></th>
+              <th><button type="button" className="two" onClick={input}>2</button></th>
+              <th><button type="button" className="three" onClick={input}>3</button></th>
+              <th><button type="button" className="multiply" onClick={input}>x</button></th>
+            </tr>
+            <tr>
+              <th><button type="button" className="four" onClick={input}>4</button></th>
+              <th><button type="button" className="five" onClick={input}>5</button></th>
+              <th><button type="button" className="six" onClick={input}>6</button></th>
+              <th><button type="button" className="minus" onClick={input}>-</button></th>
+            </tr>
+            <tr>
+              <th><button type="button" className="seven" onClick={input}>7</button></th>
+              <th><button type="button" className="eight" onClick={input}>8</button></th>
+              <th><button type="button" className="nine" onClick={input}>9</button></th>
+              <th><button type="button" className="plus" onClick={input}>+</button></th>
+            </tr>
+            <tr>
+              <th colSpan={2}><button type="button" className="zero" onClick={input}>0</button></th>
+              <th><button type="button" className="dot" onClick={input}>.</button></th>
+              <th><button type="button" className="equal" onClick={input}>=</button></th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      <p className="quote">
+        Pure mathematics is, in its way, the poetry of logical ideas.
+        ~ Albert Einstein
+      </p>
+    </div>
+  );
+}
+export default Quote;
